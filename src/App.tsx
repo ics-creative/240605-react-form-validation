a68d89dded3d071f8085600f860adfd34d8035d9
ics-konno <konno@ics-web.jp>
2024-06-05 19:32:06 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -9,66 +9,66 @@ import { ReactSimpleFormSample } from "./sections/ReactSimpleFormSample.tsx";
 import { SchemaFormSample } from "./sections/SchemaFormSample.tsx";
 
 function App() {
-	return (
-		<>
-			<section id="#section-1" className="section">
-				<div className="container is-max-desktop">
-					<div className="content">
-						<h2>Reactを使用したフォーム</h2>
-						<ReactSimpleFormSample />
-						<details>
-							<summary>コードを見る</summary>
-							<div className="content">
-								<pre>{ReactSimpleFormSampleCode}</pre>
-							</div>
-						</details>
-					</div>
-				</div>
-			</section>
-			<section id="#section-2" className="section">
-				<div className="container is-max-desktop">
-					<div className="content">
-						<h2>React Hook Formを使用したフォーム</h2>
-						<ReactHookFormSample />
-						<details>
-							<summary>コードを見る</summary>
-							<div className="">
-								<pre>{ReactHookFormSampleCode}</pre>
-							</div>
-						</details>
-					</div>
-				</div>
-			</section>
-			<section id="#section-3" className="section">
-				<div className="container is-max-desktop">
-					<div className="content">
-						<h2>Zodを使用したフォーム</h2>
-						<SchemaFormSample />
-						<details>
-							<summary>コードを見る</summary>
-							<div className="">
-								<pre>{SchemaFormSampleCode}</pre>
-							</div>
-						</details>
-					</div>
-				</div>
-			</section>
-			<section id="#section-4" className="section">
-				<div className="container is-max-desktop">
-					<div className="content">
-						<h2>Zodを使用した相関チェック</h2>
-						<CorrelationCheckSample />
-						<details>
-							<summary>コードを見る</summary>
-							<div className="">
-								<pre>{CorrelationCheckSampleCode}</pre>
-							</div>
-						</details>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section id="#section-1" className="section">
+        <div className="container is-max-desktop">
+          <div className="content">
+            <h2>Reactを使用したフォーム</h2>
+            <ReactSimpleFormSample />
+            <details>
+              <summary>コードを見る</summary>
+              <div className="content">
+                <pre>{ReactSimpleFormSampleCode}</pre>
+              </div>
+            </details>
+          </div>
+        </div>
+      </section>
+      <section id="#section-2" className="section">
+        <div className="container is-max-desktop">
+          <div className="content">
+            <h2>React Hook Formを使用したフォーム</h2>
+            <ReactHookFormSample />
+            <details>
+              <summary>コードを見る</summary>
+              <div className="">
+                <pre>{ReactHookFormSampleCode}</pre>
+              </div>
+            </details>
+          </div>
+        </div>
+      </section>
+      <section id="#section-3" className="section">
+        <div className="container is-max-desktop">
+          <div className="content">
+            <h2>Zodを使用したフォーム</h2>
+            <SchemaFormSample />
+            <details>
+              <summary>コードを見る</summary>
+              <div className="">
+                <pre>{SchemaFormSampleCode}</pre>
+              </div>
+            </details>
+          </div>
+        </div>
+      </section>
+      <section id="#section-4" className="section">
+        <div className="container is-max-desktop">
+          <div className="content">
+            <h2>Zodを使用した相関チェック</h2>
+            <CorrelationCheckSample />
+            <details>
+              <summary>コードを見る</summary>
+              <div className="">
+                <pre>{CorrelationCheckSampleCode}</pre>
+              </div>
+            </details>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- src/sections/CorrelationCheckSample.tsx
+++ src/sections/CorrelationCheckSample.tsx
@@ -3,18 +3,18 @@ import { type SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
 const dateSchema = z
-	.object({
-		startDate: z.string().date("日付を入力してください"),
-		endDate: z.string().date("日付を入力してください"),
-	})
-	.refine((arg) => new Date(arg.startDate) < new Date(arg.endDate), {
-		message: "終了日は開始日より後の日付を入力してください",
-		path: ["endDate"],
-	});
+  .object({
+    startDate: z.string().date("日付を入力してください"),
+    endDate: z.string().date("日付を入力してください"),
+  })
+  .refine((arg) => new Date(arg.startDate) < new Date(arg.endDate), {
+    message: "終了日は開始日より後の日付を入力してください",
+    path: ["endDate"],
+  });
 
 const schema = z.object({
-	title: z.string().min(1, { message: "タイトルを入力してください" }),
-	date: dateSchema,
+  title: z.string().min(1, { message: "タイトルを入力してください" }),
+  date: dateSchema,
 });
 
 // スキーマから型を生成
@@ -24,66 +24,66 @@ type Inputs = z.infer<typeof schema>;
  * 相関チェックのサンプルです
  */
 export const CorrelationCheckSample = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		trigger,
-		getValues,
-	} = useForm<Inputs>({
-		resolver: zodResolver(schema),
-		mode: "onTouched",
-	});
-	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		window.alert(
-			`タイトル：${data.title}}\n日付：${data.date.startDate}~${data.date.endDate}`,
-		);
-	};
-	return (
-		<form className="box" onSubmit={handleSubmit(onSubmit)}>
-			<div className="field">
-				<label className="label">タイトル</label>
-				<div className="control">
-					<input className="input" type="text" {...register("title")} />
-				</div>
-				{errors.title && (
-					<p className="help is-danger">{errors.title.message}</p>
-				)}
-			</div>
-			<div className="field">
-				<label className="label">開始日</label>
-				<div className="control">
-					<input
-						className="input"
-						type="date"
-						{...register("date.startDate")}
-						onBlur={() => {
-							if (getValues("date.endDate") !== "") {
-								trigger("date.endDate");
-							}
-						}}
-					/>
-				</div>
-				{errors.date?.startDate && (
-					<p className="help is-danger">{errors.date.startDate?.message}</p>
-				)}
-			</div>
-			<div className="field">
-				<label className="label">終了日</label>
-				<div className="control">
-					<input className="input" type="date" {...register("date.endDate")} />
-				</div>
-				{errors.date?.endDate && (
-					<p className="help is-danger">{errors.date.endDate.message}</p>
-				)}
-			</div>
-			<div className="field">
-				<div className="control">
-					<button className="button is-link" type="submit">
-						submit
-					</button>
-				</div>
-			</div>
-		</form>
-	);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+    getValues,
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+    mode: "onTouched",
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    window.alert(
+      `タイトル：${data.title}}\n日付：${data.date.startDate}~${data.date.endDate}`,
+    );
+  };
+  return (
+    <form className="box" onSubmit={handleSubmit(onSubmit)}>
+      <div className="field">
+        <label className="label">タイトル</label>
+        <div className="control">
+          <input className="input" type="text" {...register("title")} />
+        </div>
+        {errors.title && (
+          <p className="help is-danger">{errors.title.message}</p>
+        )}
+      </div>
+      <div className="field">
+        <label className="label">開始日</label>
+        <div className="control">
+          <input
+            className="input"
+            type="date"
+            {...register("date.startDate")}
+            onBlur={() => {
+              if (getValues("date.endDate") !== "") {
+                trigger("date.endDate");
+              }
+            }}
+          />
+        </div>
+        {errors.date?.startDate && (
+          <p className="help is-danger">{errors.date.startDate?.message}</p>
+        )}
+      </div>
+      <div className="field">
+        <label className="label">終了日</label>
+        <div className="control">
+          <input className="input" type="date" {...register("date.endDate")} />
+        </div>
+        {errors.date?.endDate && (
+          <p className="help is-danger">{errors.date.endDate.message}</p>
+        )}
+      </div>
+      <div className="field">
+        <div className="control">
+          <button className="button is-link" type="submit">
+            submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
